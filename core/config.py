5b95e89d39e2edddcef9fd6f7fb11a1cedc391d5
Michael Eco <michael.gunawan2002@gmail.com>
2025-10-23 21:37:38 +0700

    feat: implemented DefaultSettings and Settings scaffolding

---
+++ core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class DefaultSettings(BaseSettings):
+    model_config: SettingsConfigDict = SettingsConfigDict(
+        env_file=".env", extra="ignore", frozen=True, env_nested_delimiter="__"
+    )
+
+
+class Settings(DefaultSettings):
+    app_version: str = "0.1.0"
+    debug: bool = True
+    environment: str = "development"
+    service_name: str = "churn-api"
