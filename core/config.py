3d8efaac4705558fde17775c93d4cbc64bb2c599
Michael Eco <michael.gunawan2002@gmail.com>
2025-10-29 20:42:51 +0700

    feat: added feature config

--- core/config.py
+++ core/config.py
@@ -14,3 +14,30 @@ class Settings(DefaultSettings):
 
 def get_settings() -> Settings:
     return Settings()
+
+FEATURES_TO_USE = [
+    "Age",
+    "Gender",
+    "Support Calls",
+    "Payment Delay",
+    "Total Spend",
+    "Last Interaction"
+]
+
+TARGET = "Churn"
+
+# --- Feature Engineering Bins ---
+# For "Age"
+AGE_BINS = [17, 24, 39, 59, 100]  # Bins: (17-24], (24-39], (39-59], (59-100]
+AGE_LABELS = ["Young Adult", "Adult", "Mid-Career", "Senior"]
+
+# For "Last Interaction"
+INTERACTION_BINS = [-1, 7, 15, 30] # Bins: (0-7], (7-15], (15-30]
+INTERACTION_LABELS = ["Highly Active", "Active", "At Risk", "Dormant"]
+
+# --- Encoding Mappings ---
+# Ordinal features and their correct order
+ORDINAL_FEATURES_MAP = {
+    "Age_Binned": AGE_LABELS,
+    "Interaction_Binned": INTERACTION_LABELS
+}
