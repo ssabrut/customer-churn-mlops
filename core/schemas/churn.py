b8e4b66a96f457efb71440f3cb7398506d923330
Michael Eco <michael.gunawan2002@gmail.com>
2025-11-07 15:14:01 +0700

    feat: added schemas for churn request and response

---
+++ core/schemas/churn.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+
+class ChurnRequest(BaseModel):
+    Age: float = Field(..., alias="Customer Age")
+    Support_Calls: float  = Field(..., alias="Support Calls")
+    Payment_Delay: float = Field(..., alias="Payment Delay")
+    Total_Spend: float = Field(..., alias="Total Spend")
+    Last_Interaction: float = Field(..., alias="Last Interaction")
+    Gender: str = Field("Customer Gender")
+
+    class Config:
+        populate_by_name = True
+        json_schema_extra = {
+            "example": {
+                "Age": 23.0,
+                "Gender": "Male",
+                "Support Calls": 0.0,
+                "Payment Delay": 19.0,
+                "Total Spend": 846.18,
+                "Last Interaction": 18.0
+            }
+        }
+
+class ChurnResponse(BaseModel):
+    churn_prediction: int
+    version: str
